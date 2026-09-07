3016d6141089a79aaf9743db9d09faa2c864ad8a
nayanasm14 <nayanasm14@gmail.com>
2026-09-08 00:10:06 +0530

    Update __init__.py

--- search/__init__.py
+++ search/__init__.py
@@ -1,5 +1,10 @@
 """Web/social evidence discovery module."""
 
-from .search import search_web
+
+def search_web(*args, **kwargs):
+    from .search import search_web as _search_web
+
+    return _search_web(*args, **kwargs)
+
 
 __all__ = ["search_web"]
